d91133e99072495f55368104f672fb4f19c7cec8
Shane Lennon <shane.lennon36@mail.dcu.ie>
2023-02-21 12:55:31 +0000

    adding some front end

---
+++ src/Web-App/React-Native-App/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "bingoapp5b6f642a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
