cd932e2678bce34b380b2c89bcb570a3d0add417
Shane Lennon <shane.lennon36@mail.dcu.ie>
2023-02-23 14:48:35 +0000

    changing to typescript to fit with industry standards

--- src/Web-App/bingo-app/src/index.tsx
+++ src/Web-App/bingo-app/src/index.tsx
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '@aws-amplify/ui-react/styles.css'
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-import { AmplifyProvider } from '@aws-amplify/ui-react';
-
-Amplify.configure(awsExports);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-  <AmplifyProvider>
+  <React.StrictMode>
     <App />
-  </AmplifyProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
